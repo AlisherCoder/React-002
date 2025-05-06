058500886822875cd2a86706e89f33f2b9c2c9d8
alisher <alishersharipov670.gmail.com>
2025-05-06 05:12:29 +0500

    first commit

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Menu = () => {
    return (
       <section className="py-2.5 h-16 bg-white flex justify-center items-center">
-         <ul className="container flex justify-between px-11 text-[#768088]">
-            <li className="text-[14px]">
+         <ul className="container flex justify-between px-11 text-[#768088] text-[14px]">
+            <li>
                <a href="#">Nature</a>
             </li>
             <li>
